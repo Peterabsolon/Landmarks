4ff2187d8501e8f5e112bf5f723e125c520a536d
Peter Absolon <peterabsolon@yahoo.com>
2022-07-04 13:42:10 +0200

    fix: stuff

---
+++ Landmarks/Model/ModelData.swift
@@ -0,0 +1,35 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Peter Absolon on 04.07.2022.
+//
+
+import Foundation
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    // Try to read file
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle")
+    }
+    
+    // Try to load data from file
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+    
+    // Try to parse data
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
